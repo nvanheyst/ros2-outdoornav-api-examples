fc8e29f8b20835ca99fe9bdfe0974f09275d0e95
Nathan Van Heyst <nqvanheyst@gmail.com>
2026-06-23 13:00:12 -0400

    fix for shellcheck

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# shellcheck source=/dev/null
 source /opt/ros/jazzy/setup.bash
 if [ -f /opt/ros2_overlay/install/setup.bash ]; then
+  # shellcheck source=/dev/null
   source /opt/ros2_overlay/install/setup.bash
 fi
 
